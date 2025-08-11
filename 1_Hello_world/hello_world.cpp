194173940479328e41e1ba94f9f2c56b1cdc7e3b
Mayn <1665947418@qq.com>
2025-08-11 16:18:12 +0800

    2

---
+++ 1_Hello_world/hello_world.cpp
@@ -0,0 +1,18 @@
+// #include "iostream"
+// using namespace std;
+//
+// int main()
+// {
+//     cout<<"Hello world! 你好"<<endl;
+//     return 0;
+// }
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Hello world! 你好" << endl;
+    return 0;
+}
