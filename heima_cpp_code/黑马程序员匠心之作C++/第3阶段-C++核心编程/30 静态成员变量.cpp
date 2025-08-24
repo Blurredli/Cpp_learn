85723feabca73c1189409f9f9d86e6d9ac7221ca
Mayn <1665947418@qq.com>
2025-08-24 15:44:10 +0800

    8.24

---
+++ heima_cpp_code/黑马程序员匠心之作C++/第3阶段-C++核心编程/30 静态成员变量.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 16659 on 25-8-24.
+//
+#include <iostream>
+using namespace std;
+
+class Person
+{
+public:
+    static int m_A; // 静态成员变量
+    //静态成员变量特点：
+    //1 在编译阶段分配内存
+    //2 类内声明，类外初始化
+    //3 所有对象共享同一份数据
+private:
+    static int m_B; //静态成员变量也有访问权限
+};
+
+//类外初始化
+int Person::m_A = 10;
+int Person::m_B = 20;
+
+void test01()
+{
+    //1 通过对象访问
+    Person p1;
+    p1.m_A = 100;
+    cout << "p1.m_A = " << p1.m_A << endl;
+    Person p2;
+    p2.m_A = 200;
+    cout << "p1.m_A = " << p1.m_A << endl;
+    cout << "p2.m_A = " << p2.m_A << endl;
+    //2 通过类名访问
+    cout << "Person::m_A = " << Person::m_A << endl;
+
+    //cout << "m_B = " << Person::m_B << endl; //私有权限访问不到
+
+}
+
+int main ()
+{
+    test01();
+
+    return 0;
+}
